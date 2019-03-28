aae900f5daf4ae54071425bfbeedd0763e55d495
Jimmy Young <yangjufo@gmail.com>
2019-03-28 11:41:31 -0700

    day4

---
+++ Java/268. Missing Number.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int res = nums.length;
+        for (int i = 0; i < nums.length; i++)
+            res ^= nums[i] ^ i;                
+        return res;
+    }
+}
