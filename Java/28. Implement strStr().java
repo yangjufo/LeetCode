a50fe06f3977060683e9f9f0853d24a50678f446
Jimmy Young <yangjufo@gmail.com>
2019-07-11 20:50:18 -0700

    day14-2

---
+++ Java/28. Implement strStr().java
@@ -0,0 +1,17 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        if (needle.isEmpty()) return 0;        
+        for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
+            boolean find = true;
+            for (int j = 0; j < needle.length(); j++) {
+                if (haystack.charAt(i + j) != needle.charAt(j)) {
+                    find = false;
+                    break;
+                }
+            }
+            if (find == true)
+                return i;
+        }
+        return -1;
+    }
+}
