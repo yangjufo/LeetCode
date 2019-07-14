69ed260bc8e5e1aa1fd7cdf752e643e1bee46050
Jimmy Young <yangjufo@gmail.com>
2019-07-13 20:39:55 -0700

    Weekly Contest 145

---
+++ Python/1122. Relative Sort Array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        arr1Map = dict()
+        for a1 in arr1:
+            if a1 not in arr1Map:
+                arr1Map[a1] = 1
+            else:
+                arr1Map[a1] += 1
+        res = list()
+        for a2 in arr2:
+            res += [a2] * arr1Map[a2]
+            del(arr1Map[a2])
+        left = sorted(arr1Map)                  
+        for a2 in left:
+            res += [a2] * arr1Map[a2]
+        return res
