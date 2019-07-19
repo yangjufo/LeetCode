cc9a2a4059b4612d6ecd6049d332c981741f7a66
Jimmy Young <yangjufo@gmail.com>
2019-07-18 22:37:52 -0700

    format a java file

--- Java/1. Two Sum.java
+++ Java/1. Two Sum.java
@@ -1,14 +1,14 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {        
-        Map<Integer, Integer> numIndex = new HashMap<>();        
-        int [] res = new int[2];
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numIndex = new HashMap<>();
+        int[] res = new int[2];
         for (int i = 0; i < nums.length; i++) {
             if (numIndex.containsKey(target - nums[i])) {
                 res[0] = numIndex.get(target - nums[i]);
-                res[1] = i ;
+                res[1] = i;
                 break;
             }
-            numIndex.put(nums[i], i);        
+            numIndex.put(nums[i], i);
         }
         return res;
     }
