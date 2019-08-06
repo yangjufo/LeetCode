c2f8fecfb2e5a826b0c29f5a0c4889b3c62d4ece
Jimmy Young <yangjufo@gmail.com>
2019-08-05 22:27:02 -0700

    day19-1

---
+++ Python/70. Climbing Stairs.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        steps = [0] * (n + 1)
+        steps[0] = 1
+        steps[1] = 1
+        for i in range(2, n + 1):
+            steps[i] = steps[i - 1] + steps[i - 2]
+        return steps[n]
+        
