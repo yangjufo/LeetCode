cff0a26cc3a8a23e27cf2c53c0d0e6b2d530586e
Jimmy Young <yangjufo@gmail.com>
2019-09-02 15:38:47 -0700

    day28-2

--- Python/21. Merge Two Sorted Lists.py
+++ Python/21. Merge Two Sorted Lists.py
@@ -1,28 +1,32 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 
-class Solution:
-    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        dummy_head = ListNode(0)
-        prev = dummy_head
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        l3 = ListNode(0)
+        pre = l3
         while l1 != None or l2 != None:
-            if l1 != None and l2 != None:
-                if l1.val < l2.val:
-                    l3 = ListNode(l1.val)
-                    l1 = l1.next
-                else:
-                    l3 = ListNode(l2.val)
-                    l2 = l2.next
-            elif l1 != None:
-                l3 = ListNode(l1.val)
+            if l1 == None:
+                curr = l2
+                l2 = l2.next
+            elif l2 == None:
+                curr = l1
+                l1 = l1.next
+            elif l1.val < l2.val:
+                curr = l1
                 l1 = l1.next
             else:
-                l3 = ListNode(l2.val)
+                curr = l2
                 l2 = l2.next
-            prev.next = l3
-            prev = prev.next
-        return dummy_head.next
+            pre.next = curr
+            pre = curr
+        return l3.next
