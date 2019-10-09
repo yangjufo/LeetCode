f3644e6fd6776f55965d3195e0c19c5a679b0870
Jimmy Young <yangjufo@gmail.com>
2019-10-08 20:24:06 -0700

    day35-18

---
+++ Python/684. Redundant Connection.py
@@ -0,0 +1,31 @@
+class DSU:
+    def __init__(self):
+        self.par = [x for x in range(1001)]
+        self.rnk = [0] * 1001        
+    
+    def find(self, x):
+        if self.par[x] != x:
+            self.par[x] = self.find(self.par[x])
+        return self.par[x]
+    
+    def union(self, x, y):
+        xr = self.find(x)
+        yr = self.find(y)
+        if xr == yr:
+            return False
+        elif self.rnk[xr] < self.rnk[yr]:
+            self.par[xr] = yr
+        elif self.rnk[xr] > self.rnk[yr]:
+            self.par[yr] = xr
+        else:
+            self.par[yr] = xr
+            self.rnk[xr] += 1
+        return True
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        dsu = DSU()
+        for edge in edges:
+            if not dsu.union(*edge):
+                return edge
+        
