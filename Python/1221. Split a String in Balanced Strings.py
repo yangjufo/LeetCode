ed7685e503794260df4d389ef780d18b68d9e2bb
Jimmy Young <yangjufo@gmail.com>
2019-10-13 08:54:21 -0700

    Weekly Contest 158-1

---
+++ Python/1221. Split a String in Balanced Strings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def balancedStringSplit(self, s: str) -> int:
+        count = 0
+        ans = 0
+        for c in s:
+            if c == 'L':
+                count -= 1
+            else:
+                count += 1
+            if count == 0:
+                ans += 1
+        return ans
