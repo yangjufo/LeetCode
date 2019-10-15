85fe1ae3b9fe3c29791aa3d96f8f44cc19aba08c
Jimmy Young <yangjufo@gmail.com>
2019-10-15 10:33:20 -0700

    day42-1

---
+++ Python/767. Reorganize String.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reorganizeString(self, S: str) -> str:
+        N = len(S)
+        A = []
+        for c, x in sorted((S.count(x), x) for x in set(S)):
+            if c > (N + 1) / 2:
+                return ""
+            A.extend(c * x)
+        ans = [None] * N
+        ans[::2] = A[N//2:]
+        ans[1::2] = A[:N//2]
+        return "".join(ans)
