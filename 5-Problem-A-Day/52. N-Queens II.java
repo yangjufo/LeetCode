c36b1275f34ffe3b0e919c37163a8944de94d80d
Jimmy Young <yangjufo@gmail.com>
2019-10-30 14:32:44 -0700

    20191030

---
+++ 5-Problem-A-Day/52. N-Queens II.java
@@ -0,0 +1,51 @@
+class Solution {
+    int rows[];
+    int hills[];
+    int dales[];
+    int n;
+    int ans;
+    int queens[];
+
+    private boolean isNotUnderAttack(int row, int col) {
+        int res = rows[col] + hills[row - col + 2 * n] + dales[row + col];
+        return (res == 0) ? true : false;
+    }
+
+    private void placeQueen(int row, int col) {
+        queens[row] = col;
+        rows[col] = 1;
+        hills[row - col + 2 * n] = 1;
+        dales[row + col] = 1;
+    }
+
+    private void removeQueen(int row, int col) {
+        queens[row] = 0;
+        rows[col] = 0;
+        hills[row - col + 2 * n] = 0;
+        dales[row + col] = 0;
+    }
+
+
+    private void backtrack(int row) {
+        for (int col = 0; col < n; col++) {
+            if (isNotUnderAttack(row, col)) {
+                placeQueen(row, col);
+                if (row + 1 == n)
+                    ans += 1;
+                else
+                    backtrack(row + 1);
+                removeQueen(row, col);
+            }
+        }
+    }
+
+    public int totalNQueens(int n) {
+        this.n = n;
+        rows = new int[n];
+        hills = new int[4 * n - 1];
+        dales = new int[2 * n - 1];
+        queens = new int[n];
+        backtrack(0);
+        return ans;
+    }
+}
