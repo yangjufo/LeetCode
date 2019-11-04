a11dd637b37be696c1b714a72e53526f00b9f0cb
Jimmy Young <yangjufo@gmail.com>
2019-11-03 21:18:58 -0800

    20191103

---
+++ 5-Problem-A-Day/81. Search in Rotated Sorted Array II.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+                return true;
+
+            if (nums[left] == nums[mid] && nums[right] == nums[mid]) {
+                left++;
+                right--;
+            } else if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && nums[mid] > target)
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            } else {
+                if (nums[mid] < target && nums[right] >= target)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+        }
+        return false;
+
+    }
+}
