7b455088ef504cf971f380536aadb2183d7678e5
Jimmy Young <yangjufo@gmail.com>
2019-11-07 09:34:23 -0800

    20191107

---
+++ 5-Problem-A-Day/110. Balanced Binary Tree.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode left; TreeNode
+ * right; TreeNode(int x) { val = x; } }
+ */
+class Solution {
+    private int getDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        int left = getDepth(root.left);
+        int right = getDepth(root.right);
+        if (left == -1 || right == -1 || Math.abs(left - right) > 1)
+            return -1;
+        return Math.max(left, right) + 1;
+    }
+
+    public boolean isBalanced(TreeNode root) {
+        return getDepth(root) != -1;
+    }
+}
