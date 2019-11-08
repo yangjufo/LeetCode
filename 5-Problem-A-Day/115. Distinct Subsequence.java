160ad71b36c9459038e17d14ea3ed3d09779310f
Jimmy Young <yangjufo@gmail.com>
2019-11-08 10:36:04 -0800

    20191108

---
+++ 5-Problem-A-Day/115. Distinct Subsequence.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        int m = s.length(), n = t.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i < m + 1; i++)
+            dp[i][0] = 1;
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1))
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+        return dp[m][n];
+    }
+}
