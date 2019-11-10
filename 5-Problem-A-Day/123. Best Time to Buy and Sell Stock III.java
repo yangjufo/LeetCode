510d6cad5304602d0db5350ba0871184643b45bd
Jimmy Young <yangjufo@gmail.com>
2019-11-09 19:35:33 -0800

    20191109

---
+++ 5-Problem-A-Day/123. Best Time to Buy and Sell Stock III.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int oneBuy = Integer.MAX_VALUE, twoBuy = Integer.MAX_VALUE;
+        int oneProfit = 0, twoProfit = 0;
+        for (int p : prices) {
+            oneBuy = Math.min(oneBuy, p);
+            oneProfit = Math.max(oneProfit, p - oneBuy);
+            twoBuy = Math.min(twoBuy, p - oneProfit);
+            twoProfit = Math.max(twoProfit, p - twoBuy);
+        }
+        return twoProfit;
+    }
+}
