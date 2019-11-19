1d57b534493e967471e321e2da028239d83163cc
Jimmy Young <yangjufo@gmail.com>
2019-11-18 19:50:27 -0800

    20191118

---
+++ 5-Problem-A-Day/178. Rank Scores.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT
+  Score,
+  @rank := @rank + (@prev <> (@prev := Score)) Rank
+FROM
+  Scores,
+  (SELECT @rank := 0, @prev := -1) init
+ORDER BY Score desc
