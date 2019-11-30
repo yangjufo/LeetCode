1fcc696221e7300ff6e27a14d4eadbeb03b2bff0
Jimmy Young <yangjufo@gmail.com>
2019-11-29 16:02:50 -0800

    20191129

---
+++ 5-Problem-A-Day/255. Verify Preorder Sequence in Binary Search Tree.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean verifyPreorder(int[] preorder) {
+        int low = Integer.MIN_VALUE;
+        Stack<Integer> stack = new Stack<>();
+        for (int p : preorder) {
+            if (p < low)
+                return false;
+            while (!stack.empty() && p > stack.peek())
+                low = stack.pop();
+            stack.push(p);
+        }
+        return true;
+    }
+}
