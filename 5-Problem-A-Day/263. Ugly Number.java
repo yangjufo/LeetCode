1215fbafe9c19d927e92122cb7481e711817b0fd
Jimmy Young <yangjufo@gmail.com>
2019-12-02 14:13:34 -0800

    20191201

---
+++ 5-Problem-A-Day/263. Ugly Number.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean isUgly(int num) {       
+        while (num % 2 == 0 && num > 0) num /= 2;
+        while (num % 3 == 0 && num > 0) num /= 3;
+        while (num % 5 == 0 && num > 0) num /= 5;        
+        return num == 1;
+    }
+}
