c0fc907c64e9a5521174d232ee45dc082c924157
Jimmy Young <yangjufo@gmail.com>
2020-02-12 15:16:52 -0800

    20200212

---
+++ 5-Problem-A-Day/320. Generalized Abbreviation.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<String> generateAbbreviations(String word) {
+        List<String> ans = new LinkedList<>();
+        backtrack(ans, new StringBuilder(), word, 0, 0);
+        return ans;
+    }
+
+    private void backtrack(List<String> ans, StringBuilder builder, String word, int i, int k) {
+        int len = builder.length();
+        if (i == word.length()) {
+            if (k != 0)
+                builder.append(k);
+            ans.add(builder.toString());
+        } else {
+            backtrack(ans, builder, word, i + 1, k + 1);
+            if (k != 0)
+                builder.append(k);
+            builder.append(word.charAt(i));
+            backtrack(ans, builder, word, i + 1, 0);
+        }
+        builder.setLength(len);
+    }
+}
