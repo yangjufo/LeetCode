c7443fe2d27847708c10f8049d31e2cf0a737e21
Jimmy Young <yangjufo@gmail.com>
2020-04-05 16:21:56 -0700

    30-Day LeetCoding Challenge_Week1_3

---
+++ 2020/Move Zeroes.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int start = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[start] = nums[i];
+                start += 1;
+            }                
+        }
+        for (int i = start; i < nums.length; i++)
+            nums[i] = 0;
+    }
+}
