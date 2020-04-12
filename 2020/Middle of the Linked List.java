fb99eef269ae1073da55a19cc8f3fbabc049f2d5
Jimmy Young <yangjufo@gmail.com>
2020-04-11 18:04:17 -0700

    30-Day LeetCoding Challenge_Week2_1

---
+++ 2020/Middle of the Linked List.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode slow, fast;
+        slow = fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return fast.next == null ? slow : slow.next;
+    }
+}
