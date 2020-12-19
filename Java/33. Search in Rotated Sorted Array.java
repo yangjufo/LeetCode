76e90208eaa09251c0afaed15609b63db3b8045b
Jimmy Young <yangjufo@gmail.com>
2020-12-19 14:28:20 -0800

    move one file

--- Java/33. Search in Rotated Sorted Array.java
+++ Java/33. Search in Rotated Sorted Array.java
@@ -1,30 +1,26 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int start = 0, end = nums.length - 1, mid = -1;
-        int realStart = 0;
-        while (start <= end) {
-            mid = start + (end - start) / 2;            
-            if ((mid == 0 || nums[mid] < nums[mid - 1]) && (mid == nums.length - 1 || nums[mid] < nums[mid + 1])) {                
-                break;
-            }
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
             if (nums[mid] > nums[nums.length - 1])
-                start = mid + 1;
+                left = mid + 1;
             else
-                end = mid - 1;
-        }        
-        realStart = mid;
-        start = 0;
-        end = nums.length - 1;        
-        while (start <= end) {
-            mid = start + (end - start) / 2;
-            int realMid = (mid + realStart) % nums.length;            
+                right = mid;
+        }
+        int realStart = left;
+        left = 0;
+        right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int realMid = (mid + realStart) % nums.length;
             if (nums[realMid] == target)
                 return realMid;
-            if (nums[realMid] > target)
-                end = mid - 1;
+            else if (nums[realMid] > target)
+                right = mid - 1;
             else
-                start = mid + 1;
-        }        
-        return -1;        
+                left = mid + 1;
+        }
+        return -1;
     }
 }
