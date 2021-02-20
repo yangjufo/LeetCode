02360a3b4e37f44d8270f91fdcc528f9b6a537dd
Jimmy Young <yangjufo@gmail.com>
2021-02-19 20:55:48 -0800

    20210219

--- 2021/1249. Minimum Remove to Make Valid Parentheses.cpp
+++ 2021/1249. Minimum Remove to Make Valid Parentheses.cpp
@@ -56,3 +56,69 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {        
+        int openCount = 0, closeCount = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(') {
+                openCount++;
+            }
+            if (s[i] == ')' && openCount > closeCount) {
+                closeCount++;
+            }
+        }
+        
+        int openKeep = min(openCount, closeCount);
+        openCount = 0, closeCount = 0;
+        string str;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(') {
+                if (openCount < openKeep) {                    
+                    str += s[i];
+                    openCount++;
+                }
+            } else if (s[i] == ')') {
+                if (openCount > closeCount) {
+                    str += s[i];
+                    closeCount++;
+                }
+            } else {
+                str += s[i];
+            }
+        }
+        return str;
+    }
+};
+
+
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack<int> pStack;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(') {
+                pStack.push(i);
+            } else if (s[i] == ')') {
+                if (pStack.empty()) {
+                    s[i] = '#';
+                } else {
+                    pStack.pop();
+                }
+            }
+        }
+        while (!pStack.empty()) {
+            s[pStack.top()] = '#';
+            pStack.pop();
+        }
+        string res;
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] != '#') {
+                res += s[i];
+            }               
+        }
+        return res;
+    }
+};
