3755e116579a6d9b3f9ddf02ba1e9496c06e1b40
Jimmy Young <yangjufo@gmail.com>
2021-03-19 23:17:28 -0700

    20210319

--- 2021/841. Keys and Rooms.cpp
+++ 2021/841. Keys and Rooms.cpp
@@ -25,48 +25,6 @@ public:
 };
 
 
-class HitCounter {
-public:
-    int hitCounter[300];
-    int timestamps[300];
-    /** Initialize your data structure here. */
-    HitCounter() {
-        fill(hitCounter, hitCounter + 300, 0);
-        fill(timestamps, timestamps + 300, 0);
-    }
-    
-    /** Record a hit.
-        @param timestamp - The current timestamp (in seconds granularity). */
-    void hit(int timestamp) {
-        int index = timestamp % 300;
-        if (timestamps[index] != timestamp) {
-            timestamps[index] = timestamp;
-            hitCounter[index] = 1;
-        } else {
-            hitCounter[index]++;
-        }
-    }
-    
-    /** Return the number of hits in the past 5 minutes.
-        @param timestamp - The current timestamp (in seconds granularity). */
-    int getHits(int timestamp) {
-        int hits = 0;
-        for (int i = 0; i < 300; i++) {
-            if (timestamp - timestamps[i] < 300) {
-                hits += hitCounter[i];
-            }
-        }
-        return hits;
-    }
-};
-
-/**
- * Your HitCounter object will be instantiated and called as such:
- * HitCounter* obj = new HitCounter();
- * obj->hit(timestamp);
- * int param_2 = obj->getHits(timestamp);
- */
-
 class Solution {
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
@@ -90,5 +48,21 @@ public:
         }
         
         return true;
+    }
+};
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        unordered_set<int> visited;
+        dfs(visited, rooms, 0);
+        return visited.size() == rooms.size();
+    }
+    
+    void dfs(unordered_set<int>& visited, vector<vector<int>>& rooms, int index) {
+        if (visited.find(index) != visited.end()) return;
+        visited.insert(index);
+        for (int key : rooms[index]) {
+            dfs(visited, rooms, key);
+        }
     }
 };
