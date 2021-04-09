5f285781550e3e80186f45d3ee4bce1756b399ec
Jimmy Young <yangjufo@gmail.com>
2021-04-08 23:37:05 -0700

    20210407

--- 2021/1704. Determine if String Halves Are Alike.cpp
+++ 2021/1704. Determine if String Halves Are Alike.cpp
@@ -13,3 +13,21 @@ public:
         return leftCount == rightCount;
     }
 };
+
+class Solution {
+public:
+    unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+    bool halvesAreAlike(string s) {
+        int leftCount = 0, rightCount = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (vowels.find(s[i]) != vowels.end()) {
+                if (i >= s.length() / 2) {
+                    rightCount++;
+                } else {
+                    leftCount++;
+                }
+            }
+        }
+        return leftCount == rightCount;
+    }
+};
