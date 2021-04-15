cb3ba79c462b8faf1eb077a3e3fb72e7f62775f6
Jimmy Young <yangjufo@gmail.com>
2021-04-14 22:58:56 -0700

    20210414

--- 2021/86. Partition List.cpp
+++ 2021/86. Partition List.cpp
@@ -30,3 +30,36 @@ public:
         return head;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode *leftHead = new ListNode(), *rightHead = new ListNode();
+        ListNode *leftPrev = leftHead, *rightPrev = rightHead;
+        while (head != NULL) {
+            if (head->val < x) {
+                leftPrev->next = head;
+                leftPrev = head;
+            } else {
+                rightPrev->next = head;
+                rightPrev = head;
+            }            
+            head = head->next;
+        }
+        leftPrev->next = rightHead->next;
+        rightPrev->next = NULL;
+        head = leftHead->next;
+        delete leftHead, rightHead;
+        return head;
+    }
+};
