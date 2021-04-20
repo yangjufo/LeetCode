745443ec577a73fc39731e0ad386191158a2fb14
Jimmy Young <yangjufo@gmail.com>
2021-04-19 21:57:11 -0700

    20210419

--- 2021/377. Combination Sum IV.cpp
+++ 2021/377. Combination Sum IV.cpp
@@ -19,3 +19,23 @@ public:
          return count;
     }
 };
+
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        vector<int> dp(target + 1, 0);
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {            
+            for (int n : nums) {
+                if (n <= i) {
+                    if (dp[i] > INT_MAX - dp[i - n]) {
+                        dp[i] = INT_MAX;
+                    } else {
+                        dp[i] += dp[i - n];
+                    }                    
+                }
+            }    
+        }
+        return dp[target];
+    }
+};
