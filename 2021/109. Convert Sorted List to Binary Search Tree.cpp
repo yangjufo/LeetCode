9db2330820732376ef98879749b0a37c066da567
Jimmy Young <yangjufo@gmail.com>
2021-05-06 21:49:11 -0700

    20210506

--- 2021/109. Convert Sorted List to Binary Search Tree.cpp
+++ 2021/109. Convert Sorted List to Binary Search Tree.cpp
@@ -86,3 +86,49 @@ public:
         return root;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* head;
+    TreeNode* sortedListToBST(ListNode* head) {
+        this->head = head;
+        int size = 0;
+        while (head != NULL) {
+            head = head->next;
+            size++;
+        }
+        return sortedListToBST(0, size - 1);
+    }
+    
+    TreeNode* sortedListToBST(int left, int right) {
+        if (left > right) return NULL;
+        int mid = left + (right - left) / 2;
+        TreeNode* leftRoot = sortedListToBST(left, mid - 1);
+        TreeNode* root = new TreeNode(head->val);
+        root->left = leftRoot;
+        head = head->next;
+        root->right = sortedListToBST(mid + 1, right);
+        return root;
+    }
+};
