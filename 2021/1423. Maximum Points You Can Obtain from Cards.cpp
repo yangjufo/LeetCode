9fb2c8a6158bad7c33aa2faccf0fb26d1e515364
Jimmy Young <yangjufo@gmail.com>
2021-05-11 19:04:13 -0700

    20210511

--- 2021/1423. Maximum Points You Can Obtain from Cards.cpp
+++ 2021/1423. Maximum Points You Can Obtain from Cards.cpp
@@ -34,3 +34,22 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        int n = cardPoints.size();
+        vector<int> preSum = {0};
+        int sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += cardPoints[i];
+        }
+        int res = sum;
+        for (int i = k - 1; i >= 0; i--) {
+            sum -= cardPoints[i];
+            sum += cardPoints[n - (k - i)];            
+            res = max(res, sum);
+        }
+        return res;
+    }
+};
