ea58da9b86dbdabaefd8425d40621d0ab9a1d6f7
Jimmy Young <yangjufo@gmail.com>
2021-05-30 14:12:10 -0700

    20210530

--- 2021/164. Maximum Gap.cpp
+++ 2021/164. Maximum Gap.cpp
@@ -40,3 +40,34 @@ public:
         return max(maxDiff, maxVal - previous);
     }
 };
+
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        if (nums.size() <= 1) return 0;        
+        int minVal = nums[0], maxVal = nums[0];
+        for (int n : nums) {
+            minVal = min(n, minVal);
+            maxVal = max(n, maxVal);
+        }                
+        
+        int N = nums.size(), gap = (int)ceil((maxVal - minVal) * 1.0 / (N - 1));
+        int bucketMin[N - 1], bucketMax[N - 1];
+        fill(bucketMin, bucketMin + (N - 1), INT_MAX);
+        fill(bucketMax, bucketMax + (N - 1), INT_MIN);
+        for (int n : nums) {
+            if (n == minVal || n == maxVal) continue;
+            int index = (n - minVal) / gap;
+            bucketMin[index] = min(bucketMin[index], n);
+            bucketMax[index] = max(bucketMax[index], n);
+        }
+        
+        int previous = minVal, maxDiff = 0;
+        for (int i = 0; i < N - 1; i++) {
+            if (bucketMin[i] == INT_MAX) continue;
+            maxDiff = max(bucketMin[i] - previous, maxDiff);
+            previous = bucketMax[i];
+        }
+        return max(maxDiff, maxVal - previous);
+    }
+};
