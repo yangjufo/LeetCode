97f783cfe4e9bf77c99cdaa68235f5d4d3b60eaa
Jimmy Young <yangjufo@gmail.com>
2021-06-01 20:41:31 -0700

    20210601

--- 2021/695. Max Area of Island.cpp
+++ 2021/695. Max Area of Island.cpp
@@ -28,3 +28,32 @@ public:
         }
     }
 };
+
+class Solution {
+public:
+    int directions[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int maxArea = 0;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == 1) {
+                    maxArea = max(maxArea, dfs(grid, i, j));
+                }
+            }
+        }
+        return maxArea;
+    }
+    
+    int dfs(vector<vector<int>>& grid, int x, int y) {
+        grid[x][y] = 0;
+        int area = 1;
+        for (int i = 0; i < 4; i++) {
+            int newX = x + directions[i][0], newY = y + directions[i][1];
+            if (newX < 0 || newX >= grid.size() || newY < 0 || newY >= grid[0].size() || grid[newX][newY] != 1) {
+                continue;
+            }
+            area = max(area, area + dfs(grid, newX, newY));
+        }
+        return area;
+    }
+};
