02a05e2d32d49fdd33893960bd389cb8d0b20d9a
Jimmy Young <yangjufo@gmail.com>
2021-06-02 23:00:38 -0700

    20210602

--- 2021/97. Interleaving String.cpp
+++ 2021/97. Interleaving String.cpp
@@ -18,3 +18,57 @@ public:
         return mem[start1][start2] == 1 ? true : false;
     }
 };
+
+
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        if (s1.length() + s2.length() != s3.length()) return false;
+        if (s3.empty()) return true;
+        vector<vector<bool>> dp(s1.length() + 1, vector<bool>(s2.length() + 1, false));         
+        for (int i = 0; i <= s1.length(); i++) {            
+            for (int j = 0; j <= s2.length(); j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = true;
+                } 
+                if (i == 0 && j != 0) {
+                    dp[i][j] = dp[i][j - 1] && s2[j - 1] == s3[i + j - 1];
+                }
+                if (j == 0 && i != 0) {
+                    dp[i][j] = dp[i - 1][j] && s1[i - 1] == s3[i + j - 1];
+                } 
+                if (i != 0 && j != 0) {
+                    dp[i][j] = (dp[i][j - 1] && s2[j - 1] == s3[i + j - 1]) || (dp[i - 1][j] && s1[i - 1] == s3[i + j - 1]);
+                }
+            }
+        }
+        return dp[s1.length()][s2.length()];
+    }        
+        
+};
+
+class Solution {
+public:
+    bool isInterleave(string s1, string s2, string s3) {
+        if (s1.length() + s2.length() != s3.length()) return false;        
+        vector<bool> dp(s2.length() + 1, false);
+        for (int i = 0; i <= s1.length(); i++) {            
+            for (int j = 0; j <= s2.length(); j++) {
+                if (i == 0 && j == 0) {
+                    dp[j] = true;
+                } 
+                if (i == 0 && j != 0) {
+                    dp[j] = dp[j - 1] && s2[j - 1] == s3[i + j - 1];
+                }
+                if (j == 0 && i != 0) {
+                    dp[j] = dp[j] && s1[i - 1] == s3[i + j - 1];
+                } 
+                if (i != 0 && j != 0) {
+                    dp[j] = (dp[j - 1] && s2[j - 1] == s3[i + j - 1]) || (dp[j] && s1[i - 1] == s3[i + j - 1]);
+                }
+            }
+        }
+        return dp[s2.length()];
+    }        
+        
+};
