a93f6a1f25dfc7d46805d1c4d71643c84928b551
Jimmy Young <yangjufo@gmail.com>
2021-06-09 21:48:31 -0700

    20210609

--- 2021/1696. Jump Game VI.cpp
+++ 2021/1696. Jump Game VI.cpp
@@ -18,3 +18,22 @@ public:
         return dp[nums.size() - 1];
     }
 };
+
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+        int n = nums.size();
+        deque<pair<int, int>> maxes; // value, index
+        for (int i = 0; i < nums.size(); i++) {
+            while (!maxes.empty() && maxes.back().second + k < i) {
+                maxes.pop_back();              
+            }            
+            int currMax = (maxes.empty() ? 0 : maxes.back().first) + nums[i];
+            while (!maxes.empty() && maxes.front().first <= currMax) {
+                maxes.pop_front();
+            }
+            maxes.push_front({currMax, i});
+        }
+        return maxes.front().first;
+    }
+};
