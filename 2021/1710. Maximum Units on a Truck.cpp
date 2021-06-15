10c53d9659035f0944a4f2f48d9194d5c4dce54b
Jimmy Young <yangjufo@gmail.com>
2021-06-14 19:37:21 -0700

    20210614

--- 2021/1710. Maximum Units on a Truck.cpp
+++ 2021/1710. Maximum Units on a Truck.cpp
@@ -15,3 +15,20 @@ public:
         return total;
     }
 };
+
+class Solution {
+public:
+    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) {
+        sort(boxTypes.begin(), boxTypes.end(), [](vector<int>& left, vector<int>& right) {
+           return left[1] > right[1]; 
+        });
+        int total = 0;
+        for (vector<int>& box : boxTypes) {
+            if (truckSize <= 0) break;
+            int pick = min(truckSize, box[0]);
+            truckSize -= pick;
+            total += pick * box[1];
+        }
+        return total;
+    }
+};
