572975deaa53db98d09383ae6fd14bf1531540cc
Jimmy Young <yangjufo@gmail.com>
2021-06-16 22:50:18 -0700

    202106016

--- 2021/22. Generate Parentheses.cpp
+++ 2021/22. Generate Parentheses.cpp
@@ -46,3 +46,25 @@ public:
         }
     }
 };
+
+class Solution {
+public:
+    vector<string> res;
+    vector<string> generateParenthesis(int n) {
+        backtrack(0, 0, n, "");
+        return res;
+    }
+    
+    void backtrack(int open, int close, int n, string curr) {
+        if (close >= n) {
+            res.push_back(curr);
+            return;
+        }
+        if (open < n) {
+            backtrack(open + 1, close, n, curr + "(");            
+        }
+        if (close < open) {
+            backtrack(open, close + 1, n, curr + ")");
+        }       
+    }
+};
