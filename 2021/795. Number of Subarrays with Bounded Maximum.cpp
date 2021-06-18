0c36feb6555821e7303302c396c7ecb483f53fbc
Jimmy Young <yangjufo@gmail.com>
2021-06-17 20:21:26 -0700

    20210617

---
+++ 2021/795. Number of Subarrays with Bounded Maximum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
+        int start = -1, dp = 0, total = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > 0 && nums[i] < left) {
+                total += dp;
+            } else if (nums[i] > right) {
+                dp = 0;
+                start = i;
+            } else if (nums[i] >= left && nums[i] <= right) {
+                dp = i - start;
+                total += dp;
+            }
+        }
+        return total;
+    }    
+};
