2fecd89ea28d42a76bd1233b7b21b6d88b563c6d
Jimmy Young <yangjufo@gmail.com>
2021-09-22 20:11:56 -0700

    20210922

--- 2021/317. Shortest Distance from All Buildings.cpp
+++ 2021/317. Shortest Distance from All Buildings.cpp
@@ -43,7 +43,6 @@ public:
                 for (int j = 0; j < 4; j++) {
                     int nX = pos[0] + directions[j][0], nY = pos[1] + directions[j][1];
                     if (nX < 0 || nX >= m || nY < 0 || nY >= n || grid[nX][nY] != 0 || visited[nX][nY]) continue;   
-                    cout << "what" << endl;
                     dis[nX][nY] += currDis;                
                     reach[nX][nY]++;       
                     visited[nX][nY] = true;
@@ -52,6 +51,63 @@ public:
             }
             currDis++;
         }        
+    }
+};
+
+class Solution {
+public:
+    int shortestDistance(vector<vector<int>>& grid) {
+        int m = grid.size(); 
+        int n = grid[0].size();
+
+        int mindist = INT_MAX;
+        vector<vector<int>> distance(m, vector<int>(n,0));
+        
+        int target = 0;
+        
+        vector<vector<int>> dir {{1,0},{0,1}, {-1,0},{0,-1}};
+        
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(grid[i][j] != 1) continue;
+                
+                mindist = INT_MAX;
+                queue<pair<int,int>> q;
+                q.emplace(i,j);
+                int level = 1;
+                while(!q.empty())
+                {
+                    int sz = q.size();
+                    for(int k = 0; k < sz; k++)
+                    {
+                        auto curr = q.front();
+                        q.pop();
+                        for(auto &d : dir)
+                        {
+                            int a = curr.first+d[0];
+                            int b = curr.second+d[1];
+                            
+                            if(a == m || b == n || a == -1 || b == -1) continue;
+                            
+                            if(grid[a][b] == target)
+                            {
+                                q.emplace(a,b);
+                                grid[a][b]--;
+                                distance[a][b] += level;
+                                mindist = min(mindist, distance[a][b]);
+                            }
+                        }
+                    }
+                    level++;
+                }
+                target--;
+            }
+        }
+        
+        return mindist == INT_MAX ? -1 : mindist;
+        
     }
 };
 
