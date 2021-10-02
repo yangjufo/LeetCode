f62841984d9d27c5d2739f0f5baf966badb30577
Jimmy Young <yangjufo@gmail.com>
2021-10-01 21:33:27 -0700

    20211001

--- 2021/174. Dungeon Game.cpp
+++ 2021/174. Dungeon Game.cpp
@@ -17,3 +17,23 @@ public:
         return 1 - dp[0][0];
     }
 };
+
+class Solution {
+public:
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        int m = dungeon.size(), n = dungeon[0].size();
+        int dp[m][n];``
+        memset(dp, sizeof(dp), 0);        
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {                
+                dp[i][j] = (i == m - 1)
+                    ? ((j == n - 1) ? dungeon[i][j] : dp[i][j + 1] + dungeon[i][j])
+                    : ((j == n - 1) ? dp[i + 1][j] + dungeon[i][j] : max(dp[i + 1][j], dp[i][j + 1]) + dungeon[i][j]);
+                if (dp[i][j] > 0) {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        return 1 - dp[0][0];
+    }
+};
