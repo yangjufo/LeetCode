d035b5559b541ed0261509a3daa62be096217773
Jimmy Young <yangjufo@gmail.com>
2021-11-04 23:24:32 -0700

    20211104

--- 2021/441. Arranging Coins.cpp
+++ 2021/441. Arranging Coins.cpp
@@ -13,3 +13,19 @@ public:
         return (1 + left) * left / 2 > n ? left - 1 : left;
     } 
 };
+
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        long long left = 0, right = INT_MAX;
+        while (left < right) {
+            long long  mid = left + (right - left) / 2;
+            if (((1 + mid) * mid  + 1) / 2 < n) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return ((1 + left) * left + 1) / 2 <= n ? left : left - 1;
+    }
+};
