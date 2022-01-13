07b0a96a575db36e1d384c00cf733ab2a5735717
Jimmy Young <yangjufo@gmail.com>
2022-01-12 21:27:38 -0800

    20220112

--- 2021/452. Minimum Number of Arrows to Burst Balloons.cpp
+++ 2021/452. Minimum Number of Arrows to Burst Balloons.cpp
@@ -3,7 +3,24 @@ public:
     int findMinArrowShots(vector<vector<int>>& points) {
         sort(points.begin(), points.end());
         int i = 0, count = 0;
-        while (i < points.size()) {            
+        while (i < points.size()) {
+            count++;
+            int minEnd = points[i][1];
+            while (i < points.size() && points[i][0] <= minEnd) {
+                minEnd = min(points[i][1], minEnd);
+                i++;
+            }
+        }
+        return count;
+    }
+};
+
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        sort(points.begin(), points.end());
+        int i = 0, count = 0;
+        while (i < points.size()) {
             count++;
             int minEnd = points[i][1];
             while (i < points.size() && points[i][0] <= minEnd) {
