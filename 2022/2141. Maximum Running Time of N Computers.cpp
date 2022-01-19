1ec09c3c96c3a1e24976f85ce51c9bc4ecc90536
Jimmy Young <yangjufo@gmail.com>
2022-01-18 21:32:14 -0800

    20210118

--- 2022/2141. Maximum Running Time of N Computers.cpp
+++ 2022/2141. Maximum Running Time of N Computers.cpp
@@ -1,25 +1,11 @@
 class Solution {
 public:
-    long long maxRunTime(int n, vector<int>& batteries) {
-        priority_queue<long long, vector<long long>, greater<long long>> pq;
-        sort(batteries.begin(), batteries.end(), greater<int>());
-        vector<long long> lb;
-        for (int b : batteries) {
-            lb.push_back(b);
-        }
-        long long sum = 0;
-        for (int i = n; i < lb.size(); i++) {
-            sum += lb[i];
-        }
-        int i = n - 1;
-        for (; i >= 1 && sum > n - i; i--) {
-            long long diff = min(lb[i - 1] - lb[i], sum / (n - i));
-            lb[i] += diff;
-            sum -= diff * (n - i);
-        }
-        if (i == 0 || i == n - 1) {
-            return lb[i] + sum / n;
-        }
-        return lb[i + 1] + sum / (n - i);
+    long long maxRunTime(int n, vector<int>& A) {
+        sort(A.begin(), A.end());
+        long long sum = accumulate(A.begin(), A.end(), 0L);
+        int k = 0, na = A.size();
+        while (A[na - 1 - k] > sum / (n - k))
+            sum -= A[na - 1 - k++];
+        return sum / (n - k);
     }
 };
