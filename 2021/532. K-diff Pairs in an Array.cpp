3e9811692b0fba84481b4a0123630920435a2959
Jimmy Young <yangjufo@gmail.com>
2022-02-08 19:58:07 -0800

    20220208

--- 2021/532. K-diff Pairs in an Array.cpp
+++ 2021/532. K-diff Pairs in an Array.cpp
@@ -5,7 +5,7 @@ public:
         for (int n : nums) {
             numCount[n]++;
         }
-        
+
         int count = 0;
         for (auto iter : numCount) {
             if (k > 0 && numCount.find(iter.first + k) != numCount.end()) {
@@ -13,6 +13,25 @@ public:
             } else if (k == 0 && iter.second > 1) {
                 count++;
             }
+        }
+        return count;
+    }
+};
+
+class Solution {
+public:
+    int findPairs(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        unordered_set<int> numSet;
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if ((k == 0 && i >= 2 && nums[i] == nums[i - 2]) || (k != 0 && i >= 1 && nums[i] == nums[i - 1])) {
+                continue;
+            }
+            if (numSet.find(nums[i] - k) != numSet.end()) {
+                count++;
+            }
+            numSet.insert(nums[i]);
         }
         return count;
     }
