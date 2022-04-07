f41407d1e91a9d377f52de4e5f0864fc68a1e4f3
Jimmy Young <yangjufo@gmail.com>
2022-04-06 22:23:18 -0700

    20220406

---
+++ 2022/1046. Last Stone Weight.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq(stones.begin(), stones.end());
+        while (pq.size() > 1) {
+            int first = pq.top();
+            pq.pop();
+            int second = pq.top();
+            pq.pop();
+            if (first - second > 0) {
+                pq.push(first - second);
+            }
+        }
+        return pq.empty() ? 0 : pq.top();
+    }
+};
