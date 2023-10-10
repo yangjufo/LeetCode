e297bf85bb12f93e53756f465f137202841e95ca
Jimmy Young <yangjufo@gmail.com>
2023-10-09 21:34:04 -0700

    Updat a wrong solution.

--- 2023/72. Edit Distance.cpp
+++ 2023/72. Edit Distance.cpp
@@ -6,10 +6,10 @@ public:
             return m + n;
         }
         int dp[m + 1][n + 1];
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i <= m; i++) {
             dp[i][0] = i;
         }
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j <= n; j++) {
             dp[0][j] = j;
         }
         for (int i = 1; i <= m; i++) {
